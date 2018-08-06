91a295a183f4e4d73132a8703b27e7a973a2b903
XebiaNetCore <pkonar@xebia.com>
2018-08-06 15:15:33 +0530

    Added delete /todo/id route

--- server/server.js
+++ server/server.js
@@ -45,6 +45,22 @@ app.get("/todos/:id",(req,res) => {
  });
 });
 
+app.delete("/todos/:id",(req,res) => {
+ var id = req.params.id;
+ if(!ObjectID.isValid(id)){
+     return res.status(400).send("Invalid Request ID...");
+ }
+
+ ToDo.findByIdAndRemove(id).then((todo) => {
+   if(!todo){
+       return res.status(404).send();
+   }
+   res.send({todo});
+ },(err) => {
+     return res.status(400).send(err);
+ });
+});
+
 app.listen(port,()=>{
     console.log(`The server is up and running on port ${port}`);
 });
