078fb84d9950bbd66d3a774bd80087f80a1b6fc7
XebiaNetCore <pkonar@xebia.com>
2018-08-17 14:16:03 +0530

    Added test for DELETE  /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -348,3 +348,25 @@ describe("POST /users/login",()=>{
        });
     });
 });
+
+describe("DELETE /users/me/token",() => {
+  it("should delete the token",(done) => {
+      request(app)
+      .delete("/users/me/token")
+      .set("x-auth",users[0].tokens[0].token)
+      .expect(200)
+      .end((err,res)=>{
+        if(err){
+            return done(err);
+        }
+        User.findById({
+            _id : users[0]._id
+        }).then((user)=>{
+           expect(user.tokens.length).toEqual(0);
+           done();
+        }).catch((err) => {
+            done(err);
+        });
+      });
+  });
+});
